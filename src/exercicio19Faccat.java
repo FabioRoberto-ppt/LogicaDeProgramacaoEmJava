ffc51bf98eb6e8c7102669d04e8f716d0e5497b3
= <=>
2026-01-08 19:24:04 -0300

    exercicio 19 adicionado

--- src/exercicio19Faccat.java
+++ src/exercicio19Faccat.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
+
 public class exercicio19Faccat {
+    public static void main (String []args){
+        System.out.println("19) Ler dois valores (considere que não serão lidos valores iguais) e escrever o maior deles. ");
+        System.out.println("");
+
+        Scanner leia = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número: ");
+        int primeiroNumero = leia.nextInt();
+        System.out.println("");
+
+        System.out.println("Digite o segundo número: ");
+        int segundoNumero = leia.nextInt();
+        System.out.println("");
+
+        if (primeiroNumero > segundoNumero) {
+            System.out.println("O primeiro numero é maior " + primeiroNumero);
+        }
+        else{
+            System.out.println("O Segundo numero é maior " + segundoNumero);
+        }
+    }
 }
