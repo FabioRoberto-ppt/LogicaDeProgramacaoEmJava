236584c9d49974cf1a6e9db94a0a6a17f3a7fee0
= <=>
2026-01-08 19:15:01 -0300

    dois exercicos adicionados

--- src/exercicio18Faccat.java
+++ src/exercicio18Faccat.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class exercicio18Faccat {
     public static void main (String []args){
+        System.out.println("18) Ler o ano atual e o ano de nascimento de uma pessoa. Escrever uma mensagem que diga se ela \n" +
+                "poderá ou não votar este ano (não é necessário considerar o mês em que a pessoa nasceu).");
+        System.out.println("");
 
+        Scanner leia = new Scanner(System.in);
+
+        System.out.println("Digite qual ano você nasceu : ");
+        int anos = leia.nextInt();
+
+        int anosFinal = 2026 - anos;
+
+        if(anos >= 18){
+            System.out.println("Você tem " + anosFinal + " e pode votar");
+        }
+        else {
+            System.out.println("Você tem " + anosFinal + " e não pode votar até ter 18 anos");
+        }
     }
 }
