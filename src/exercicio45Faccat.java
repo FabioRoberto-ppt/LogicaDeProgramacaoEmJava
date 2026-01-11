61418a48587c47212095698749f085ad24c91cf3
= <=>
2026-01-11 20:58:01 -0300

    adicionado exercicio 46

--- src/exercicio45Faccat.java
+++ src/exercicio45Faccat.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class exercicio45Faccat {
+    public static void main (String []args){
+        System.out.println("");
+        System.out.println("");
+
+        Scanner leia = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro valor: ");
+        int numero1 = leia.nextInt();
+
+        int numero2 = 0;
+
+
+        while(numero2 == 0){
+         System.out.println("Digite o segundo valor: ");
+         numero2 = leia.nextInt();
+        }
+
+        System.out.println("O Resultado é: " + numero1 / numero2);
+    }
 }
--- src/exercicio46Faccat.java
+++ src/exercicio46Faccat.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class exercicio46Faccat {
+    public static void main (String []args){
+        System.out.println("46) Acrescentar uma mensagem de 'VALOR INVÁLIDO' no exercício [44] caso o segundo valor \n" +
+                "informado seja ZERO.");
+        System.out.println("");
+
+        Scanner leia = new Scanner(System.in);
+
+        int segundoNumero;
+        int primeiroNumero;
+
+        do{
+        System.out.println("Digite o primeiro valor: ");
+        primeiroNumero = leia.nextInt();
+
+
+        System.out.println("Digite o segundo valor: ");
+        segundoNumero = leia.nextInt();
+
+        if(segundoNumero==0){
+            System.out.println("valor invalido");
+        }
+
+        }while(segundoNumero == 0);
+
+        System.out.println("o valor da conta é: "+ primeiroNumero / segundoNumero );
+
+    }
 }
